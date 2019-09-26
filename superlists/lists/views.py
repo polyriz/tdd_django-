8c9f6e4a75084c0de2350efa418a9e9b1416218b
Yuwon Shin <yuwon.shin2@gmail.com>
2019-09-25 21:08:16 -0700

    Functional test new checks we can input a to-do item

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>To-do lists</title></html>')
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 
 
 
